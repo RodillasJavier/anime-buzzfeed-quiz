2ca54fc2356f17ba28bc6c09eea27847a7f5fa0b
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-12 23:25:27 -0400

    Got the JS portion of the lab up and working

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-$('#favorite_beverage').on('click', 
-    function(e) {
+$('#get-results-button').on('click', function(e) {
         // gather all checked radio-button values
-        var choices = $("input[type='radio']:checked").map(
-            function(i, radio) {
-                return $(radio).val();
-            }
-        ).toArray();
-        console.log(choices);
+        var choices = $("input[type='radio']:checked").map(function(i, radio) {
+            return $(radio).val();
+        }).toArray();
+
+        var result = choices[0];
+        console.log(choices[0]);
+        document.getElementById('results-text').textContent = result;
+});
+
+/* MODAL HANDLING */
+
+// Get out modal, button and span (close button for modal)
+var modal = document.getElementById('result-modal');
+var button = document.getElementById('get-results-button');
+var span = document.getElementsByClassName('close')[0];
+
+// Opening the modal
+button.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Closing the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-);
+}
