3c0c466dec54410d1a8e06651e896a7b4623ef2a
RodillasJavier <103291127+RodillasJavier@users.noreply.github.com>
2025-04-14 19:28:48 -0400

    Updated main js

--- main.js
+++ main.js
@@ -1,20 +1,84 @@
-/* SUBMIT BUTTON HANDLING */
-$('#get-results-button').on('click', function(e) {
-    // Store the total number of questions
-    const totalQuestions = $('.question-container').length;
+/* LOADING IN QUESTIONS VIA JSON */
+$.getJSON("data.json", function(data) {
+    // Generate header
+    const headerHTML = `
+        <img class="header-image" src="${data.headerImage}" alt="Guts Resting">
+        <h1 class="header-title">${data.title}</h1>
+    `;
+    $("#header").append(headerHTML);
 
-    // Store all checked radio-button values in an array
-    var choices = $("input[type='radio']:checked").map(function(i, radio) {
-        return $(radio).val();
-    }).toArray();
+    // Generate questions
+    const quizHTML = data.questions.map((question, index) => {
+        const answersHTML = question.answers.map(answer => `
+            <label class="answer-choice ${question.question_type === 'text' ? 'text' : ''}">
+                <img src="${answer.img_url}" class="answer-choice-image">
+                <input type="radio" name="${question.question_name}" value="${answer.outcome}">
+                ${question.question_type === 'image_text' ? 
+                    `<p class="answer-label">${answer.text}</p>` :
+                    question.question_type === 'text' ? 
+                    `<p class="answer-choice-text">${answer.text}</p>` : 
+                    ''}
+            </label>
+        `).join('');
+    
+        return `
+            <div class="question-container">
+                <h2 class="question-title">${question.question_prompt}</h2>
+                <div class="answer-choice-container">
+                    ${answersHTML}
+                </div>
+            </div>
+        `;
+    }).join('');
 
-    // Validate completion of all questions
-    if (choices.length < totalQuestions) {
-        $('#results-text').text("Please answer all questions first before clicking 'Done'!");
-    }
+    $("#quiz").html(quizHTML);
 
-    // Get and display our results
-    const results = calculateResults(choices);
+    // Do quiz handling
+    initializeQuizHandlers();
+});
+
+
+
+/* QUIZ HANDLING */
+function initializeQuizHandlers() {
+    // Answer choice state handling
+    $("input[type='radio']").change(function() {
+        const $question = $(this).closest('.question-container');
+        
+        // Remove all states
+        $question.find('.answer-choice').removeClass('selected not-selected');
+        
+        // Add 'selected' state to chosen answer
+        $(this).closest('.answer-choice').addClass('selected');
+        
+        // Add 'not-selected' state to everything else
+        $question.find('.answer-choice').not('.selected').addClass('not-selected');
+    });
+
+    // Results button handling
+    $('#get-results-button').on('click', function() {
+        // Store the total number of questions
+        const totalQuestions = $('.question-container').length;
+
+        // Store all checked radio-button values in an array
+        const choices = $("input[type='radio']:checked").map(function(i, radio) {
+            return $(radio).val();
+        }).toArray();
+        
+        // Validate completion of all questions
+        if (choices.length < totalQuestions) {
+            $('#results-text').text("Please answer all questions first before clicking 'Done'!");
+            return;
+        }
+        
+        // Get and display our results
+        const results = calculateResults(choices);
+        displayResults(results);
+    });
+}
+
+// Helper to display results when the submit button is pressed
+function displayResults(results) {
     const resultsHTML = `
         <div class="modal-content">
             <h2 id="results-header">You are <u>${results.character}</u>!</h2>
@@ -25,7 +89,7 @@ $('#get-results-button').on('click', function(e) {
     `;
 
     $('#result-modal').html(resultsHTML);
-});
+}
 
 
 
@@ -101,24 +165,8 @@ function calculateResults(choices) {
 }
 
 
-/* STATE HANDLING FOR ANSWER CHOICES */
-$(document).ready(function() {
-    $("input[type='radio']").change(function() {
-        const $question = $(this).closest('.question-container');
 
-        // Remove all states
-        $question.find('.answer-choice').removeClass('selected not-selected');
-
-        // Add 'selected' state to chosen answer
-        $(this).closest('.answer-choice').addClass('selected');
-
-        // Add 'not-selected' state to everything else
-        $question.find('.answer-choice').not('.selected').addClass('not-selected');
-    });
-});
-
-
-
+/* ------------------------------------------------------------------------- */
 /* MODAL HANDLING */
 
 // Get out modal, button and span (close button for modal)
